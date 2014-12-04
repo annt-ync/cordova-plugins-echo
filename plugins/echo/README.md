f047c3f81b11e1267203c152943a0fd71e1e84cd
An Nguyen <annt@younetco.com>
2014-12-04 18:11:32 +0700

    Fist structure

---
+++ plugins/echo/README.md
@@ -0,0 +1,3 @@
+A simple plugin
+Follow Cordova Plugin Development Guide
+http://cordova.apache.org/docs/en/4.0.0/guide_hybrid_plugins_index.md.html
---
+++ plugins/echo/www/echo.js
@@ -0,0 +1,8 @@
+/**
+ * Cordova Echo Plugin
+ * @author An Nguyen <annt@younetco.com>
+ * @version 0.1.0
+ */
+(function (window) {
+    'use strict';
+}(this));
