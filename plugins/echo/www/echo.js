b5412e1ac6d9f9aec8b7f0cf58257eb735aef056
An Nguyen <annt@younetco.com>
2014-12-05 11:05:08 +0700

    Implement javascript and Java class

--- plugins/echo/www/echo.js
+++ plugins/echo/www/echo.js
@@ -3,6 +3,15 @@
  * @author An Nguyen <annt@younetco.com>
  * @version 0.1.0
  */
-(function (window) {
+(function(window) {
     'use strict';
+    
+    var exec = require('cordova/exec');
+
+    module.exports = {
+        echo: function(message, successCallback, errorCallback) {
+            message = message || "";
+            return exec(successCallback, errorCallback, 'Echo', 'echo', [message]);
+        }
+    }
 }(this));
